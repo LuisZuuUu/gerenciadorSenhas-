1a39d084aba2712859b3af41e46573912a13fcfa
LuisZuuUu <luishenrique.c.soares@gmail.com>
2026-06-17 21:30:12 -0300

    sera que vai?

--- util.py
+++ util.py
@@ -1,41 +1,148 @@
 import sqlite3
 import os
 import string
-import random
-import tkinter as tk 
+import tkinter as tk
+from time import sleep
+from random import choice
 
+
+# CLASSE FRAME
+class Frame(tk.Frame):
+    def __init__(self, parent):
+        super().__init__(parent)
+
+    def funcao(self, widget, **opcoes):
+        return widget(self, **opcoes)
+
+
+# VARIAVEIS E FUNCOES ESTATICAS
+usuarios = {}
 caminho = os.path.join(os.getcwd(), "senhas.db")
-caracteres = string.ascii_letters + string.digits + string.punctuation 
+caracteres = string.ascii_letters + string.digits + string.punctuation
 conexao = sqlite3.connect(caminho)
 cursor = conexao.cursor()
 
-cursor.execute('''CREATE TABLE IF NOT EXISTS senhas(
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS senhas(
     site TEXT NOT NULL,
     login TEXT NOT NULL,
-    senha TEXT NOT NULL)''')
+    senha TEXT NOT NULL
+)
+''')
 
 
+def gerarSenha(tamanho):
+    senha = ''.join(choice(caracteres) for _ in range(tamanho))
+    return senha
 
 
-
-def gerar_senha(tamanho):
-    senha = []
-    for i in range(tamanho + 1):
-        senha.append(random.choice(caracteres))
-    print(''.join(senha))
-
-def guardar_informacoes(site, login, senha):  
-    cursor.execute('''INSERT INTO senhas VALUES(?, ?, ?)''', (site, login, senha))
-
-def entrada(janela):
-    entrada = tk.Entry(janela)
-    entrada.pack()
-    return entrada.get()
+def guardarInformacoes(site, login, senha):
+    cursor.execute(
+        '''INSERT INTO senhas VALUES(?, ?, ?)''',
+        (site, login, senha)
+    )
 
 
+def validarLogin(nome, senha):
+    if nome in usuarios:
+        if usuarios[nome] == senha:
+            print('Login feito com sucesso')
+        else:
+            print('Senha errada')
+    else:
+        print('Usuário não existe')
+    
 
 
+def cadastrarUsuario(nome, senha):
+    if nome in usuarios:
+        labelUsuario.config(text='Usuario já existe')
+        return
+    if not nome:
+        print('Nome vazio')
+        return 
+    if not senha:
+        print('Senha vazia')
+        return False
+
+    if senha and nome:
+        botao1.destroy()
+        botao2.pack_forget()
+        
+
+        linhaConfirmacao.pack()
+        labelSenha.config(text='Confirmar senha')
+
+        if not senha2.winfo_ismapped():    
+            senha2.pack()
+    
+        if senha2.get() == senha:
+            usuarios[nome] = senha
+    
+    botao2.pack()
+
+    
+
+    
 
 
+# CODIGO PRINCIPAL
+if __name__ == '__main__':
+    
+    janela = tk.Tk()
+    janela.title("Gerenciador de Senhas")
+    janela.geometry("300x200")
+
+    # TELA INICIAL
+    telaInicial = Frame(janela)
+    telaInicial.pack()
+    linhaConfirmacao = Frame(telaInicial)
+    senha2 = linhaConfirmacao.funcao(
+            tk.Entry,
+            show="*"
+        )
+    labelUsuario = telaInicial.funcao(
+        tk.Label,
+        text='Usuario',
+        font=('Arial', 15)
+    )
+    labelUsuario.pack()
+
+    usuario = telaInicial.funcao(tk.Entry)
+    usuario.pack()
+
+    labelSenha = telaInicial.funcao(
+        tk.Label,
+        text='Senha',
+        font=('Arial', 15)
+    )
+    labelSenha.pack()
+
+    senha = telaInicial.funcao(
+        tk.Entry,
+        show="*"
+    )
+    senha.pack()
 
 
+    botao1 = telaInicial.funcao(
+        tk.Button,
+        text='Login',
+        command=lambda: validarLogin(
+            usuario.get(),
+            senha.get()
+        )
+    )
+    botao1.pack()
+
+    botao2 = telaInicial.funcao(
+        tk.Button,
+        text='Cadastro',
+        command=lambda: cadastrarUsuario(
+            usuario.get(),
+            senha.get()
+        )
+    )
+    botao2.pack()
+
+    janela.mainloop()
