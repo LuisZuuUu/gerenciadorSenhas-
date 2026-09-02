d57b2963ddf85c48ae87bc2369c74d203777c9aa
Luís Henrique carvalho Soares <86425930+LuisZuuUu@users.noreply.github.com>
2026-09-02 17:48:54 -0300

    edicao rapida
    Refactored the application structure to use classes for Window and Frame.

--- interface.py
+++ interface.py
@@ -1,10 +1,25 @@
 from customtkinter import * 
+import dataBaser 
 
-app = CTk()
-app.geometry("300x200")
+
 set_appearance_mode("system")
 
-app.mainloop()
+class Window(CTk):
+    def __init__(self, geometry=str, title=str):
+        super().__init__()
+        self.geometry = geometry
+        self.title = str 
+
+class Frame(CTkFrame):
+    def __init__(self, window):
+        super().__init__(window)
+
+    def widget(self, type, **atbs):
+        return type(self, **atbs)
+
+loginWindow= Window("400x300", "Teste")
+
+
  
 
-
+loginWindow.mainloop()
