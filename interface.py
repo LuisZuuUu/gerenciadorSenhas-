bbce95e11155bdeb355ea53dba676cdda2e3d50b
ThinkZuUu <luishenique.c.soares@gmail.com>
2026-09-02 20:40:41 -0300

    https://github.com/LuisZuuUu/PYTHON

--- interface.py
+++ interface.py
@@ -1,10 +1,48 @@
 from customtkinter import * 
 
-app = CTk()
-app.geometry("300x200")
-set_appearance_mode("system")
+class windowLogin(CTk):
+    def __init__(self, title=str, geometry=str):
+        super().__init__()
+        self.title(title)
+        self.geometry(geometry) 
+        self.current = None
+        self.frame = self.Frame(self)
 
-app.mainloop()
- 
+class Frame(CTkFrame):
+        
+    current = False
+
+    def __init__(self, master, **atbs):
+        super().__init__(master, **atbs)
+
+
+    def widget(self, widget, **atbs):
+        return widget(self, **atbs)
+
+    def changeFrame(self, frameIn=None, **atbs):
+        if frameIn is not None:
+            frameIn.grid(**atbs)
+            self.grid_forget()
+        else:
+            self.grid(**atbs)
+
+    
+windowLogin = Window("Gerenciador Senhas", "400x200")
+windowLogin.maxsize(400, 200)
+windowLogin.resizable(width=False, height=False)
+
+# TELA INICIAL 
+frameLogin = Window.Frame(windowLogin)
+frameLogin.grid()
+
+frameLogin.widget(CTkLabel, text="Login", height=5, width=40).grid()
+loginUser = frameLogin.widget(CTkEntry)
+loginUser.grid()
+
+windowLogin.mainloop()
+
+
+
+
 
 
